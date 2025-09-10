ac03a8b43d97bad6484fc498defa48dcfe3ccefa
Ramasamy3488 <91617803+Ramasamy3488@users.noreply.github.com>
2025-09-10 15:22:42 +0530

    Add files via upload

--- src/main/java/com/example/jsondatacreate/controllers/StudentController.java
+++ src/main/java/com/example/jsondatacreate/controllers/StudentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StudentController {
 
 	private final String FILE_NAME = "student.json";
